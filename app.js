a87f9a8e844a34d1e6d83f2c26aa3827e867ad9e
florencia-belmana <florbel_03@hotmai.com>
2021-04-13 21:44:12 -0300

    ejs

--- app.js
+++ app.js
@@ -4,27 +4,20 @@ const app = express();
 
 // Configuro el directorio de recursos estáticos
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/index.html'));
-})
+//app.get('/', (req, res) => {
+  //  res.sendFile(path.join(__dirname, 'src/views/index.html'));
+//})
 
-
-// Atajamos todas las rutas que no existen
-app.get('*', (req, res) => {
-    res.send('¿Te perdiste?');
-});
-
-// 3. El servidor
 
 // Iniciar el servidor
 app.listen(3000, () => console.log('El servidor está corriendo en el puerto 3000.'));
-
 
 
 //Rutas
 
 const mainRouter = require('./src/routes/mainRouter');
 
-
 app.use('/', mainRouter);
+
