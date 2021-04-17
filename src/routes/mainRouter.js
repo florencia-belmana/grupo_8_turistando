4017cad0861369e78e85b76959914f3166ead09b
unknown <alvarosopenaf@gmail.com>
2021-04-17 11:31:09 -0300

    rutas

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -5,7 +5,6 @@ const controller = require('../controllers/mainController')
 router.get('/', controller.index);
 router.get('/contacto', controller.contact);
 router.get('/registro', controller.register);
-
 router.get('/login', controller.login);
 
 
