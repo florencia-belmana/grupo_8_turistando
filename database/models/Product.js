d9b5fb81050b01d1d3b64d6e298235994beff7f7
unknown <alvarosopenaf@gmail.com>
2021-05-04 11:42:42 -0300

    sequelize

--- database/models/Product.js
+++ database/models/Product.js
@@ -4,42 +4,42 @@ module.exports = function (sequelize, dataTypes) {
     let cols = {
       id: {
         type: dataTypes.INTEGER,
-        allowNull: false,
+       
         autoIncrement: true,
         primaryKey: true,
       },
       product_name: {
         type: dataTypes.STRING,
-        allowNull: false,
+        
         
       },
       title:{
         type: dataTypes.STRING,
-        allowNull: false,
+        
         
       },
       price: {
         type: dataTypes.DECIMAL,
-        allowNull: false,
+       
       },
       image: {
         type: dataTypes.STRING,
-        allowNull: false,
+        
       },
       description: {
         type: dataTypes.TEXT,
       },
       created_at: {
         type: dataTypes.DATE,
-        allowNull: true
+        
       },
       updated_at: {
         type: dataTypes.DATE,
-        allowNull: true
+        
       },
       deleted_at: {
         type: dataTypes.DATE,
-        allowNull: true
+        
       },
     
     };
