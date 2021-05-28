c2a936ae49e58dde297a379daa81e2928f02ba2b
unknown <alvarosopenaf@gmail.com>
2021-05-28 04:52:45 -0300

    html

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/mainController')
-const db = require ("../../database/models");
 const authMiddleware = require('../middlewares/authMiddleware');
 const userMiddleware = require('../middlewares/userMiddleware');
 
